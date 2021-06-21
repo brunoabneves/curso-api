5084df65581e4cd23aad1cf582b5c086734f6b12
Bruno <brunoabneves97@gmail.com>
2021-06-20 22:39:43 -0300

    Implementando sub recursos/ Ocorrencia

--- src/main/java/com/bruno/curso/domain/model/Cliente.java
+++ src/main/java/com/bruno/curso/domain/model/Cliente.java
@@ -5,9 +5,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Size;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
 
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
---
+++ src/main/java/com/bruno/curso/domain/service/RegistroOcorrenciaService.java
@@ -0,0 +1,24 @@
+package com.bruno.curso.domain.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.bruno.curso.domain.model.Entrega;
+import com.bruno.curso.domain.model.Ocorrencia;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Service
+public class RegistroOcorrenciaService {
+
+	private BuscaEntregaService buscaEntregaService;
+	
+	@Transactional
+	public Ocorrencia registrar(Long entregaId, String descricao) {
+		Entrega entrega = buscaEntregaService.buscar(entregaId);
+		
+		return entrega.adicionarOcorrencia(descricao);
+	}
+	
+}
